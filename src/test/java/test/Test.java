b8014c8688f2beac51f989c8aee05374f7d6cff6
lvzisun <13521018314#163.com>
2017-05-18 23:26:09 +0800

    理解web.xml

---
+++ src/test/java/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+	}
+}
